558a2da5abf48c8ec8520823dbcde4bc8b12c10a
Jonas Gamburg <jonasgamburg97@gmail.com>
2022-05-22 20:30:14 +0300

    - Added support for GET request too.

--- Sources/UniversalHTTP/UniversalHTTP.swift
+++ Sources/UniversalHTTP/UniversalHTTP.swift
@@ -12,6 +12,8 @@ protocol HTTPServiceDelegate: AnyObject {
     func errorDidOccur()
 }
 
+enum HttpMethod { case POST, GET }
+
 //MARK: - UniversalHTTP
 public struct UniversalHTTP<Model : Codable> {
     
@@ -23,38 +25,50 @@ public struct UniversalHTTP<Model : Codable> {
      - Parameter url: The URL provided as a String as to where to send the POST request.
      - Parameter complition: A complition provided to return the parsed data.
      */
-    static func post<BodyModel: Codable>(_ delegate: HTTPServiceDelegate? = nil,
-                     url: String,
-                     body: [String : Any]? = nil,
-                     bodyModel: BodyModel? = nil,
-                     httpValueForHeaderField values: [String : String]? = nil,
-                                         _ complition: SuccessComplitionHandler? = nil) {
+    static func performRequest<BodyModel: Codable>(_ delegate: HTTPServiceDelegate? = nil,
+                                                   url: String,
+                                                   body: [String : Any]?  = nil,
+                                                   httpMethod: HttpMethod = .GET,
+                                                   bodyModel: BodyModel?  = nil,
+                                                   httpValueForHeaderField values: [String : String]? = nil,
+                                                   _ complition: SuccessComplitionHandler? = nil) {
         
         guard let urlComponent = URLComponents(string: url),
               let usableUrl    = urlComponent.url else {
-            delegate?.errorDidOccur()
-            return
-        }
+                  delegate?.errorDidOccur()
+                  return
+              }
         
         var request        = URLRequest(url: usableUrl)
-        request.httpMethod = "POST"
+        request.httpMethod = httpMethod == .POST ? "POST" : "GET"
         
+        
+        //MARK: Body parameters
         if let body     = body,
            let bodyData = try? JSONSerialization.data(withJSONObject: body) {
-            request.httpBody   = bodyData
+            request.httpBody = bodyData
             
         } else if let bodyModel = bodyModel,
                   let bodyData  = try? JSONEncoder().encode(bodyModel) {
             request.httpBody = bodyData
         }
         
+        
+        //MARK: Header values
         if let headerValues = values {
             for value in headerValues {
                 request.addValue(value.value, forHTTPHeaderField: value.key)
             }
         }
         
+        
+        //MARK: Data task
         URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            if let response = response as? HTTPURLResponse {
+                print("\n~~> [UniversalHTTP] Response came back with code: \(response.statusCode)")
+            }
+            
             if let error = error {
                 print("\n~~> POST request met with an error:\n\(error)")
                 delegate?.errorDidOccur()
@@ -82,11 +96,9 @@ public struct UniversalHTTP<Model : Codable> {
      - Parameter data: The data received as part of the session to be parsed and decoded.
      */
     static func parseModel(withData data: Data) -> Model? {
-        do {
-            
-            return try JSONDecoder().decode(Model.self, from: data)
-                
-        } catch {
+        do { return try JSONDecoder().decode(Model.self, from: data) }
+        
+        catch {
             print("\n~~> Error caught: \(error)")
             return nil
         }
